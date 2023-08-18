c24582ef2605d652c5d61428786e92149f7220f9
krzysckh <krzych@420blaze.it>
2023-08-18 19:19:51 +0200

    rock solid

--- sand.lua
+++ sand.lua
@@ -1,15 +1,37 @@
 local s = require("spxe")
 
-local w = 32
+local w
+if (type(jit) == 'table') then
+  -- luajit is much faster
+  w = 128
+else
+  w = 64
+end
+
 local h = w
 
 local bg = { r = 0, g = 0, b = 255, a = 255 }
-local fg = { r = 255, g = 255, b = 0, a = 255 }
+local sand = { r = 255, g = 255, b = 0, a = 255 }
+local rock = { r = 64, g = 64, b = 64, a = 255 }
+
+-- there are no enums in lua...?
+local mode = {
+  d_sand = 0,
+  d_rock = 1,
+}
 
 local pixels = s.start("sand example", 500, 500, w, h)
+local current_mode = mode.d_sand
 
-local is_sand = function(p)
-  return p.r == fg.r and p.g == fg.g and p.b == fg.b
+local color_by_mode = function(m)
+  if m == mode.d_sand then return sand end
+  if m == mode.d_rock then return rock end
+
+  error("no such mode")
+end
+
+local is = function(p, c)
+  return p.r == c.r and p.g == c.g and p.b == c.b
 end
 
 local get = function(x, y)
@@ -37,36 +59,28 @@ local update_pixels = function()
 
   local goleft = function(x, y)
     set(x, y, bg)
-    set(x - 1, y - 1, fg)
+    set(x - 1, y - 1, sand)
   end
 
   local goright = function(x, y)
     set(x, y, bg)
-    set(x + 1, y - 1, fg)
+    set(x + 1, y - 1, sand)
   end
 
   for y = 0, h - 1 do
     for x = 0, w - 1 do
       cur = get(x, y)
-      --print("[" .. x .. ", " .. y .. "]")
-      if is_sand(pixels[cur]) then
+      if is(pixels[cur], sand) then
         if y - 1 >= 0 then
-          if (is_sand(pixels[get(x, y - 1)])) then
-            local o1 = x - 1 >= 0 and not is_sand(pixels[get(x - 1, y - 1)])
-            local o2 = x + 1 < w and not is_sand(pixels[get(x + 1, y - 1)])
-
-            if o1 == true and o2 == true then
-              if math.random() >= 0.5 then
-                goleft(x, y)
-              else
-                goright(x, y)
-              end
-            elseif o1 then goleft(x, y)
-            elseif o2 then goright(x, y)
+          if is(pixels[get(x, y - 1)], sand) then
+            if x - 1 >= 0 and is(pixels[get(x - 1, y - 1)], bg) then
+              goleft(x, y)
+            elseif x + 1 < w and is(pixels[get(x + 1, y - 1)], bg) then
+              goright(x, y)
             end
-          else
+          elseif is(pixels[get(x, y - 1)], bg) then
             set(x, y, bg)
-            set(x, y - 1, fg)
+            set(x, y - 1, sand)
           end
         end
       end
@@ -79,10 +93,13 @@ while s.run(pixels) do
   update_pixels()
   if s.key.pressed(s.keys.key_escape) then break end
   if s.key.pressed(s.keys.key_r) then fillbg() end
-  if s.mouse.down(s.mouse.buttons.button_1) then
+  if s.mouse.pressed(s.mouse.buttons.button_right) then
+    current_mode = current_mode == mode.d_sand and mode.d_rock or mode.d_sand
+  end
+  if s.mouse.down(s.mouse.buttons.button_left) then
     local pos = s.mouse.pos()
     if (pos.x < h and pos.y < w) then
-      set(pos.x, pos.y, fg)
+      set(pos.x, pos.y, color_by_mode(current_mode))
     end
   end
 end
